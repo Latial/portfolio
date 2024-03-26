85b5d1e2d2dd30d3d001f59f7bf77d26fe76bab5
Latial <simic.aleksandar.sd@gmail.com>
2024-03-27 00:19:22 +0100

    updarted HeroSection

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col container mx-auto py-4 px-12">
-        <HeroSection></HeroSection>
+    <main className="flex min-h-screen flex-col">
+        <div className="container mx-auto py-4 px-12">
+            <HeroSection/>
+        </div>
     </main>
   );
 }
