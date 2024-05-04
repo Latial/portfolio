e0780c22dbce9c8bd824a9146aecc02c9e4b5356
Latial <simic.aleksandar.sd@gmail.com>
2024-05-04 15:33:54 +0200

    added a lot

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -44,6 +44,10 @@ const HeroSection = () => {
                     <Link href="/#contact" className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-fromColor to-toColor hover:from-pink-500 hover:to-yellow-500 text-white">
                         Hire Me
                     </Link>
+                    <Link href="/#contact"
+                          className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-fromColor to-toColor hover:from-pink-500 hover:to-yellow-500 text-white">
+                        Download CV
+                    </Link>
                 </div>
                 </motion.div>
                 <motion.div
@@ -54,7 +58,7 @@ const HeroSection = () => {
                 >
                 <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                     <Image
-                        src="/images/hero-image.png"
+                        src="/images/Image1.png"
                         alt="hero image"
                         className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                         width={300}
--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,7 +1,9 @@
 "use client";
-import React from 'react'
+import React, {useState} from 'react'
 import Link from 'next/link'
 import NavLink from './NavLink'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+import MenuOverlay from './MenuOverlay'
 
 const navLink = [
     {
@@ -22,13 +24,27 @@ const navLink = [
     },
 ]
 const NavBar = () => {
+    const [navBarOpen, setNavBarOpen] = useState(false);
     return (
         <nav className="fixed top-0 left-0 right-0 z-10 bg-black bg-opacity-90">
-            <div className="flex flex-wrap items-center justify-between mx-auto px-8 pt-4">
+            <div className="flex flex-wrap items-center justify-between mx-auto px-8 pt-4 pt-2">
                 <Link href={"/"}
                       className="text-4xl text-white font-semibold bg-gradient-to-r from-fromColor to-toColor w-20 h-20 rounded flex justify-center items-center">AS</Link>
-                <div className="menu block md:w-auto" id="navbar">
-                    <ul className="flex md:flex-row p-4 md:p-0 md:space-x-8 mt-0">
+                <div className="mobile-menu block md:hidden">
+                    {
+                        !navBarOpen ?(
+                            <button onClick={() => setNavBarOpen(true)} className = "flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+                                <Bars3Icon className = "h-5 w-5"/>
+                            </button>
+                        ): (
+                            <button onClick={() => setNavBarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+                                <XMarkIcon className="h-5 w-5"/>
+                            </button>
+                        )
+                    }
+                </div>
+                <div className="menu hidden md:block md:w-auto" id="navbar">
+                    <ul className="flex md:flex-row items-center p-4 md:p-0 md:space-x-8 mt-0">
                         {
                             navLink.map((link, index) => (
                                 <li key={index}>
@@ -38,13 +54,9 @@ const NavBar = () => {
                         }
                     </ul>
                 </div>
-                <div>
-                    <Link href="/#contact"
-                          className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-fromColor to-toColor hover:from-pink-500 hover:to-yellow-500 text-white">
-                        Download CV
-                    </Link>
-                </div>
             </div>
+            {navBarOpen ? <MenuOverlay links = {navLink}/>
+                : null}
         </nav>
     );
 }
