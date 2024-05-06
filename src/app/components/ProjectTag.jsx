e2d390ba8a4f2c0d3319f4cf6682dc4ac3efb9d4
Latial <simic.aleksandar.sd@gmail.com>
2024-05-07 00:06:03 +0200

    finished project section, only to change img and names

---
+++ src/app/components/ProjectTag.jsx
@@ -0,0 +1,9 @@
+﻿import React from "react";
+
+const ProjectTag = ({name, onClick, isSelected}) => {
+    const buttonStyles = isSelected ? 'border-purple-500' : "border-slate-600 hover:border-white"
+    return (
+        <button className={`${buttonStyles} rounded-full border-2 border-purple-500 px-6 py-3 cursor-pointer`} onClick={() => onClick(name)}>{name}</button>
+    )
+}
+export default ProjectTag;
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,5 +1,8 @@
-﻿import React from "react";
+﻿"use client";
+
+import React, {useState} from "react";
 import ProjectCard from './ProjectCard';
+import ProjectTag from './ProjectTag';
 
 const projectsData = [
     {
@@ -59,13 +62,26 @@ const projectsData = [
 ];
 
 const ProjectsSection = () => {
+    const [tag, setTag] = useState("All");
+    
+    const handleTagChange = (newTag) =>{
+        setTag(newTag);
+    };
+    const filteredProjects = projectsData.filter((project) =>
+        project.tag.includes(tag)
+    );
     return (
         <>
             <h2 className= "text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">My Projects</h2>
-            <div className= "grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap:12">
-                {projectsData.map((project) => 
+            <div className = "text-white flex flex-row justify-center items-center py-6 gap-2">
+                <ProjectTag onClick = {handleTagChange} name = "All" isSelected={tag === "All"}/>
+                <ProjectTag onClick = {handleTagChange} name = "Web" isSelected={tag === "Web"}/>
+                <ProjectTag onClick = {handleTagChange} name = "Mobile" isSelected={tag === "Mobile"}/>
+            </div>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap:12">
+                {filteredProjects.map((project) =>
                 <ProjectCard key = {project.id} title = {project.title} description= {project.description} imgUrl={project.image} gitUrl={project.gitUrl} previewUrl={project.previewUrl}/>)}
-            </div>2
+            </div>
         </>
     )
 }
