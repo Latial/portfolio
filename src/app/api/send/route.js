deac804005fd6cd76c9530416aeb3e938b7e2e6b
Latial <simic.aleksandar.sd@gmail.com>
2024-05-08 01:47:55 +0200

    Started email section

---
+++ src/app/api/send/route.js
@@ -0,0 +1,21 @@
+﻿//import { EmailTemplate } from '../../../components/EmailTemplate';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+    try {
+        const data = await resend.emails.send({
+            from: 'Aleksandar <simic.aleksandar.sd@gmail.com>',
+            to: ['simic.aleksandar.sd@gmail.com'],
+            subject: 'Hello world',
+            react: (
+                <><p>Welcome</p>></>
+            )
+        });
+
+        return Response.json(data);
+    } catch (error) {
+        return Response.json({ error });
+    }
+}
---
+++ src/app/components/EmailSection.jsx
@@ -0,0 +1,8 @@
+﻿import React from "react";
+
+const EmailSection = () => {
+    return (
+        <div>EmailSection</div>
+    )
+}
+export default EmailSection;
