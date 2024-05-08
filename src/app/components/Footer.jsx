f421735adc4524fc2dd9557d27f1c54f2752d216
Latial <simic.aleksandar.sd@gmail.com>
2024-05-09 01:14:36 +0200

    Added footer and contact form

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,21 @@
+﻿import React from "react";
+import Link from "next/link";
+
+const Footer = () => {
+    return (
+        <footer className= "footer border border-t-[#33353f] border-l-transparent border-r-transparent">
+            <div className="container p-12 flex justify-between">
+                <span className = "">
+                <Link href={"/"}
+                      className="lg:text-4xl text-2xl text-white font-semibold bg-gradient-to-r from-black to-white lg:w-16 lg:h-16 w-12 h-12 rounded flex justify-center items-center">AS
+                </Link>
+                    <p>Aleksandar Simic</p>
+                </span>
+                <p className= "text-slate-600 self-center">
+                    All rights reserved
+                </p>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,20 @@ import NavBar from './components/NavBar'
 import AboutSection from './components/AboutSection'
 import AchievementsSection from "./components/AchievementsSection";
 import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from './components/EmailSection';
+import Footer from './components/Footer'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-        <NavBar></NavBar>
+        <NavBar/>
         <div className="container mt-24 mx-auto px-12 py-4">
             <HeroSection/>
             <AchievementsSection/>
             <AboutSection/>
             <ProjectsSection/>
+            <EmailSection/>
         </div>
+        <Footer/>
     </main>
   );
 }
