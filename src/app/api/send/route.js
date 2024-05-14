c555c0ee93f24c930a261248a6fc57d97862041d
Latial <simic.aleksandar.sd@gmail.com>
2024-05-14 22:03:28 +0200

    fixed bug

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -3,13 +3,13 @@ import {NextResponse} from 'next/server'
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL
+/*const fromEmail = process.env.FROM_EMAIL*/
 export async function POST(req,res) {   
     const {body} = await req.json();
     const {email, subject, message} = body
     try {
         const data = await resend.emails.send({
-            from: fromEmail,
+            from: email,
             to: ['simic.aleksandar.sd@gmail.com'],
             subject: 'Hello world',
             react: (
